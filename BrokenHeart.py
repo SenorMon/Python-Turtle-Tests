cc9a9c049bc8f3112ed387a3b6da137a4a1cfd56
SirMon <93319073+SenorMon@users.noreply.github.com>
2021-10-28 11:53:14 +0200

    BrokenHeart.py
    Happens to the best of us

---
+++ BrokenHeart.py
@@ -0,0 +1,30 @@
+import turtle
+from turtle import *
+bgcolor('black')
+speed(10)
+color ('red')
+begin_fill()
+pensize (3)
+left(50)
+forward(133)
+circle(50,200)
+right(140)
+circle(50,200)
+forward(133)
+end_fill()
+color('black')
+left(150)
+
+x = 0
+while(x < 3):
+    forward(50)
+    right(38)
+    forward(50)
+    left(50)
+    x = x + 1
+
+turtle.done()
+Warning
+
+
+
